0847e1f06b34ec364808b572d173e31462685375
Shreyansh301 <shreyanshsriv5@gmail.com>
2025-01-17 23:58:37 +0530

    Implemented Keyboard Avoiding View

--- app/signIn.js
+++ app/signIn.js
@@ -1,14 +1,31 @@
-import { View, Text, StatusBar, Image, TextInput, TouchableOpacity } from "react-native";
-import React from "react";
+import { View, Text, StatusBar, Image, TextInput, TouchableOpacity, Pressable, Alert } from "react-native";
+import React, {useRef, useState} from "react";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import Octicons from "@expo/vector-icons/Octicons";
+import { useRouter } from 'expo-router';
+import Loading from '../components/Loading'
+import CustomKeyboardView from "../components/CustomKeyboardView";
 
 export default function signIn() {
+    const router = useRouter();
+    const [loading, setLoading] = useState(false);
+
+    const emailRef = useRef("");
+    const passwordRef = useRef("");
+
+    const handleLogin = async ()=>{
+      if(!emailRef.current || !passwordRef.current){
+        Alert.alert('Sign In', "Please fill all the fields!");
+        return;
+      };
+
+      //login process
+    }
   return (
-    <View className="flex-1">
+    <CustomKeyboardView>
       <StatusBar style="dark" />
       <View
         style={{ paddingTop: hp(8), paddingHorizontal: wp(5) }}
@@ -38,6 +55,7 @@ export default function signIn() {
             >
               <Octicons name="mail" size={hp(2.7)} color="black" />
               <TextInput
+                onChangeText={value=> emailRef.current=value}
                 style={{ fontSize: hp(2) }}
                 className="flex-1 font-semibold text-neutral-600"
                 placeholder="Email address"
@@ -51,9 +69,11 @@ export default function signIn() {
                 >
                   <Octicons name="lock" size={hp(2.7)} color="black" />
                   <TextInput
+                    onChangeText={value=> passwordRef.current=value}
                     style={{ fontSize: hp(2) }}
                     className="flex-1 font-semibold text-neutral-600"
                     placeholder="Password"
+                    secureTextEntry
                     placeholderTextColor={"gray"}
                   />
                 </View>
@@ -61,22 +81,38 @@ export default function signIn() {
             </View>
 
             {/* submit button */}
-            <TouchableOpacity style={{height:hp(6.5)}} className="bg-blue-500 rounded-xl justify-center items-center">
+
+            <View>
+              {
+                loading? (
+                  <View className="flex-row justify-center">
+                      <Loading size={hp(11)} />
+                  </View>
+                ):(
+                    <TouchableOpacity onPress={handleLogin} style={{height:hp(6.5)}} className="bg-blue-500 rounded-xl justify-center items-center">
               <Text style={{fontSize: hp(2.7)}} className="text-white font-bold tracking-wider">
                 Sign In
               </Text>
             </TouchableOpacity>
+                )
+              }
+            </View>
+
+            
 
             {/* sign up text */}
 
             <View className="flex-row justify-center">
               <Text style={{fontSize: hp(1.8)}} className="font-semibold text-neutral-500">Don't have an account? </Text>
+              <Pressable onPress={()=> router.push('signUp')}>
               <Text style={{fontSize: hp(1.8)}} className="font-bold text-blue-500">Sign Up</Text>
+              </Pressable>
+              
             </View>
             
           </View>
         </View>
       </View>
-    </View>
+    </CustomKeyboardView>
   );
 }
